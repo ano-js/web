eea0bacea35dcc496c9c7858d7135ab1ffa5da8f
Calix Huang <calix.huang1@gmail.com>
2019-11-19 19:45:57 -0800

    Fixed media file bug

--- anojs_project/urls.py
+++ anojs_project/urls.py
@@ -25,6 +25,8 @@ from django.conf.urls import handler404, handler500
 urlpatterns = [
     path('admin/', admin.site.urls),
     url("", include("anojs_app.urls")),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
